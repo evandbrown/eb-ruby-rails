cc59c8b24a1e57ffa33b8f8d050f9ba246fda9fd
Evan Brown <evandbrown@gmail.com>
2013-07-15 10:59:05 -0700

    Minimal Rails app

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EbRubyRails::Application.config.session_store :cookie_store, key: '_eb-ruby-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EbRubyRails::Application.config.session_store :active_record_store
